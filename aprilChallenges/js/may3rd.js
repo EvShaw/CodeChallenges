3d7c1c6631ee88e3032df7697c7d94813578836e
Evan Shaw <e85.shaw@gmail.com>
2022-05-03 10:32:46 -0700

    updates js challenges

--- aprilChallenges/js/may3rd.js
+++ aprilChallenges/js/may3rd.js
@@ -56,49 +56,89 @@ console.log('May 3rd 2022')
 
 
 
-let array = []
+// let array = []
 
-function range(start, end, step = start < end ? 1 : -1) {
+// function range(start, end, step = start < end ? 1 : -1) {
 
-            //10 going to 1, in steps of 2
-            //step = -2
+//             //10 going to 1, in steps of 2
+//             //step = -2
 
-    if (step > 0) {
-        for (let i = start; i <= end; i += step) array.push(i)
-       // starting at 10, while 10 is less than or equal to 1, += -2
-       
-       //10 > 8, 6, 4, 2,
+//     if (step > 0) {
+//         for (let i = start; i <= end; i += step) array.push(i)
+//        // starting at 10, while 10 is less than or equal to 1, += -2
 
-
-    } else {
-        for (let i = start; i >= end; i += step) array.push(i)
-        // ------ i = -10     i >= 15    i += 3 //incrementing by 3 with every iteration
-
-        //differences: first if step is larger than 0, we start at i and go to end
-        //  if step is less than   0, we start at i and go to end 
-
-        //why the conditional?
-        // }
-        // array.push(start)
-        // array.push(end)
-        // for (let i = start; i <= end; i++) {
-        //     array.push(i)
-        // }
-
-    }
-    return array
-}
-
-function sum(arr) {
-    let total = 0;
-    for (let value of array) {
-        total += value;
-    }
-    return total;
-}
+//        //10 > 8, 6, 4, 2,
 
 
-range(10, 1, 2)
+//     } else {
+//         for (let i = start; i >= end; i += step) array.push(i)
+//         // ------ i = -10     i >= 15    i += 3 //incrementing by 3 with every iteration
 
-console.log(array)
-console.log(sum(array))
+//         //differences: first if step is larger than 0, we start at i and go to end
+//         //  if step is less than   0, we start at i and go to end 
+
+//         //why the conditional?
+//         // }
+//         // array.push(start)
+//         // array.push(end)
+//         // for (let i = start; i <= end; i++) {
+//         //     array.push(i)
+//         // }
+
+//     }
+//     return array
+// }
+
+// function sum(arr) {
+//     let total = 0;
+//     for (let value of array) {
+//         total += value;
+//     }
+//     return total;
+// }
+
+
+// range(10, 1, 2)
+
+// console.log(array)
+// console.log(sum(array))
+
+// /----FILTER: 
+//  PREP
+//P: 
+//R:
+//E:
+//P:
+// const lessThanStated = characters.filter( character => character.mass < 80 )
+
+// function gooseFilter(birds) {
+//     var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+
+//     const birdies = birds.filter((b, i) => b !== geese[i])
+
+//     console.log(birdies)
+
+
+// }
+// return an array containing all of the strings in the input array except those that match strings in geese
+
+
+// console.log(birds.filter(b => !geese.includes(b)));
+
+// Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+// The geese are any strings in the following array, which is pre-populated in your solution:
+//   ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+// For example, if this array were passed as an argument:
+//  ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+// Your function would return the following array:
+// ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+// The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+
+// function gooseFilter (birds) {
+//     var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+//    console.log(birds.filter( b => !geese.includes(b)))
+//     // return an array containing all of the strings in the input array except those that match strings in geese
+//   };
+
+//   gooseFilter(["Mallard", "Hook Bill", "Crested", "Blue Swedish"])
+
