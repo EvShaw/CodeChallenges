6c7f14daff0df9257252a157b63f3d444c34c4bf
Evan Shaw <e85.shaw@gmail.com>
2022-05-04 11:43:37 -0700

    js updated

--- aprilChallenges/js/mayThe4thBeWithYou.js
+++ aprilChallenges/js/mayThe4thBeWithYou.js
@@ -22,8 +22,49 @@ console.log('May 4th')
 
 // const heights = characters.map(character => character.height)
 
+// PREP: 
+//p: function that takes two params. using the second arg to map over the first one
+//I can expect numbers and strings to be passed in. 
+//R: they want me to return an array nested inside another array
+//E: 
 
+//   function gridMap(fn,a) {
+//     return a.map(x=>x.map(fn))
+//   }
 
-function gridMap(fn,a) {
-    return [[]];
-  }
+//   const gridMap = ((fn, a) => a.map(x => x.map(fn)))
+
+//----------------------------------------------------------Holiday VI - Shark Pontoon:
+
+// sharkDistance = distance from the shark to the pontoon. The shark will eat you if it reaches you before you escape to the pontoon.
+
+// sharkSpeed = how fast it can move in metres/second.
+
+// pontoonDistance = how far you need to swim to safety in metres.
+
+// youSpeed = how fast you can swim in metres/second.
+
+// dolphin = a boolean, if true, you can half the swimming speed of the shark as the dolphin will attack it.
+
+// The pontoon, you, and the shark are all aligned in one dimension.
+
+// If you make it, return "Alive!", if not, return "Shark Bait!".
+
+//PREP: 
+//P: A function with 5 variables that change the outcome. 
+//R: return a string statement based on a condiditional. 
+//E: 4 numbers, one boolean. 
+//P: 
+
+//check pontoon distance and shark distance. What is closer?
+
+// function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+//     dolphin ? sharkSpeed = sharkSpeed / 2 : sharkSpeed
+
+//     if (sharkDistance /sharkSpeed > pontoonDistance / youSpeed) {
+//        return "Alive!"
+//     } else {
+//         return "Shark Bait!"
+//     }
+// }
+
