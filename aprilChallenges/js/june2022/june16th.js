4a1401074f3347278da6e10d90289ff8589f6314
Evan Shaw <e85.shaw@gmail.com>
2022-06-16 10:45:01 -0700

    First challenge completed

--- aprilChallenges/js/june2022/june16th.js
+++ aprilChallenges/js/june2022/june16th.js
@@ -1,2 +1,37 @@
 const today = new Date
 console.log(today)
+
+// Check Same Case: 
+
+// Write a function that will check if two given characters are the same case.
+// If either of the characters is not a letter, return -1
+// If both characters are the same case, return 1
+// If both characters are letters, but not the same case, return 0
+// Examples
+// 'a' and 'g' returns 1
+// 'A' and 'C' returns 1
+// 'b' and 'G' returns 0
+// 'B' and 'g' returns 0
+// '0' and '?' returns -1
+
+// function sameCasing(a, b) {
+//     const key = 'abcdefghijklmnopqrstuvwxyz'.split('')
+//     const alpha = a
+//     const beta = b
+//     if(!key.includes(a.toLowerCase()) || !key.includes(b.toLowerCase())) return -1
+
+//     const checkCase = x => {
+//         if(x === x.toLowerCase()) return 'lower'
+//         if(x === x.toUpperCasE()) return 'upper'
+
+//         return checkCase(a) === checkCase(b) ? 1 : 0
+//     }
+
+//     for (let i = 0; i < key.length; i++) {
+        
+//     }
+
+
+// }
+
+// console.log(sameCasing('a', 'g'))
