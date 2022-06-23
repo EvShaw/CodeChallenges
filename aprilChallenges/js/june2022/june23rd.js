2b824c02cacb44a54ed30526e97a94164fd126fe
Evan Shaw <e85.shaw@gmail.com>
2022-06-23 09:22:36 -0700

    first challenge completed

--- aprilChallenges/js/june2022/june23rd.js
+++ aprilChallenges/js/june2022/june23rd.js
@@ -1,2 +1,11 @@
 const today = new Date
 console.log(today)
+
+
+// 7kyu: shortest word: 
+
+function findShort(s){
+    let strArr = s.split(' ').sort((a, b) => a.length - b.length)
+    return strArr[0].length
+}
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
