d2727cd8cc1750d1a64d721111aaf06cc4c4a900
Evan Shaw <e85.shaw@gmail.com>
2022-06-27 10:05:38 -0700

    file cleanup

--- aprilChallenges/js/june2022/june27th.js
+++ aprilChallenges/js/june2022/june27th.js
@@ -50,10 +50,10 @@ console.log(today)
 
 //refactor: 
 
-const getSum = (a, b) => {
-    let min = Math.min(a, b),
-        max = Math.max(a, b);
-    return (max - min + 1) * (min + max) / 2;
-  }
+// const getSum = (a, b) => {
+//     let min = Math.min(a, b),
+//         max = Math.max(a, b);
+//     return (max - min + 1) * (min + max) / 2;
+//   }
 
-console.log(getSum(0, 2))
+// console.log(getSum(0, 2))
