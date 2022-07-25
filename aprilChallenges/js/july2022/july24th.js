56ec32699218795c9d9d40fafa6839db745625a5
Evan Shaw <e85.shaw@gmail.com>
2022-07-24 20:52:53 -0700

    Code challenges completed

--- aprilChallenges/js/july2022/july24th.js
+++ aprilChallenges/js/july2022/july24th.js
@@ -6,6 +6,48 @@ console.log(today)
 //with in the funciton, use the filter method and return any arguments that have a length of 4. 
 
 
-function friend(friends){
-   return friends.filter(name => name.length == 4)
-  }
+function friend(friends) {
+    return friends.filter(name => name.length == 4)
+}
+
+//   case 'january':
+//     case 'february':
+//     case 'march':
+//       return first quarter;
+//     break;
+//     case 'april'
+
+const quarterOf = (month) => {
+    switch (month) {
+        case 1 : 
+        case 2 :
+        case 3 :
+            return 1;
+            break;
+        case 4:
+        case 5:
+        case 6:
+            return 2;
+            break;
+        case 7:
+        case 8:
+        case 9:
+            return 3;
+            break;
+        case 10:
+        case 11:
+        case 12:
+            return 4;
+            break;
+    }
+
+}
+
+// 
+// const quarterOf = (month) => {
+//     // Your code here
+//     return Math.ceil(month / 3);
+//   }
+
+// const isReallyNaN = (val) => Number.isNaN(val) ? true: false
+
