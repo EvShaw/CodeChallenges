509d99c3c6b7b36636bd03b537208c55612ba1d9
Evan Shaw <e85.shaw@gmail.com>
2022-07-25 12:04:16 -0700

    first challenge completed

--- aprilChallenges/js/july2022/july25th.js
+++ aprilChallenges/js/july2022/july25th.js
@@ -1,3 +1,14 @@
 const today = new Date
 console.log(today)
 
+// create a ternaray operator that gives pricing based of of qty. 
+// accepting a single paramater, return the price based on the qty. 
+
+//3 price points: 100, 95, 90
+
+//use ternary operator: 
+
+// function saleHotdogs(n) {
+//     return n >= 10 ? n*90 : n >= 5 && n < 10 ? n * 95 : n * 100
+// }
+// console.log(saleHotdogs(4))
