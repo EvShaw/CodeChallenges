f3c7dd825898d548a56acc1cd737b103863d4a97
Evan Shaw <e85.shaw@gmail.com>
2022-07-25 12:35:35 -0700

    Second challenge completed

--- aprilChallenges/js/july2022/july25th.js
+++ aprilChallenges/js/july2022/july25th.js
@@ -12,3 +12,18 @@ console.log(today)
 //     return n >= 10 ? n*90 : n >= 5 && n < 10 ? n * 95 : n * 100
 // }
 // console.log(saleHotdogs(4))
+
+// 6kyu: Bit Counting: 
+// take an int as an input and return the number of bits that are equal to one in the binary representation of that number: always positive numbers
+// 1234 = 10011010010, so the return = 5
+
+//take the number, convert to string, split it at zero, join back together and get the length
+
+
+countBits = n => n.toString(2).split('0').join('').length;
+
+console.log(countBits(10011010010))
+console.log(countBits(110011))
+console.log(countBits(100))
+console.log(countBits(0))
+
