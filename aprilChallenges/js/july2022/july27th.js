1705d7f78be9102975acb328e3dde63ffceec5b8
Evan Shaw <e85.shaw@gmail.com>
2022-07-27 09:46:43 -0700

    Code challenge completed

--- aprilChallenges/js/july2022/july27th.js
+++ aprilChallenges/js/july2022/july27th.js
@@ -1,3 +1,19 @@
 const today = new Date
 console.log(today)
 
+// 7kyu: Area of an arrow: 
+
+//write a function which returns the area of an arrow: 
+
+// given two integers, one for the length and one for the height, calculate the total area of the arrow: 
+//hald the base and half the height, multiplied by eachother will give the total area. 
+
+const arrrowArea = (a, b) => (a / 2) * (b / 2)
+
+// function reverseList(arr) {
+//     return arr.reverse()
+   
+
+// }
+
+// console.log(reverseList([3, 2, 1]))
