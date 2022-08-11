079ede4f57b5d9f023daa770b8da275ba87640ac
Evan Shaw <e85.shaw@gmail.com>
2022-08-11 13:38:35 -0700

    Daily code challenge completed

--- aprilChallenges/js/aug2022/aug11th.js
+++ aprilChallenges/js/aug2022/aug11th.js
@@ -1,4 +1,27 @@
 const today = new Date
 console.log(today)
-console.log(today)
 
+// 6 kyu: 
+    //define int argument, true or false if integer is 
+    //natural number greater than one, can't be divided by anything other than itself and 1
+        // 
+
+    // is_prime(1)  /* false */
+    // is_prime(2)  /* true  */
+    // is_prime(-1) /* false */
+
+    //always int input, not always positive, can be 0. 
+
+    // create the function, accept a single argument (a single integer)
+        // if statement, stating the int is <= 0, return false.
+        // the int... 
+            //  dividing it by 2, and see if you get a whole number. If you do, it can't be a prime number
+
+
+
+function isPrime(num) {
+
+
+}
+
+  //73 (prime)
