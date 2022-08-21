cec0a2279094c1a0ac1da7442c59151296a8a50f
Evan Shaw <e85.shaw@gmail.com>
2022-08-20 18:06:51 -0700

    all 8: 01/389 completed

--- aprilChallenges/js/aug2022/aug20th.js
+++ aprilChallenges/js/aug2022/aug20th.js
@@ -1,4 +1,12 @@
 const today = new Date
 console.log(today)
 
+// 1 of 389 => Completed
+// Opposites Attract
 
+function lovefunc(flower1, flower2) {
+    if (flower1 % 2 == 0 && flower2 % 2 != 0 || flower1 % 2 != 0 && flower2 % 2 == 0) {
+        return true
+    } else return false
+
+}
