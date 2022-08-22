cc45e9ce2dfe628071272b53450ebf6998563781
Evan Shaw <e85.shaw@gmail.com>
2022-08-21 17:04:54 -0700

    2x katas completed!

--- aprilChallenges/js/aug2022/aug21st.js
+++ aprilChallenges/js/aug2022/aug21st.js
@@ -12,17 +12,35 @@ console.log(today)
 // x and n
 // x is what we are counting by and n is what we are counting to. 
 
-// using a loop, start at x and count up to x by n
+// using a loop, start at x and count up to x*n in increments of x
 
-const countBy = (x, n) => {
-    const ans = []
+// const countBy = (x, n) => {
+//     const ans = []
 
-    for (let i = x; i <= x*n; i += x) {
-        ans.push(i)
+//     for (let i = x; i <= x * n; i += x) {
+//         ans.push(i)
+//     }
+//     return ans
+// }
+
+// console.log(countBy(2, 5))
+
+
+//@bonus challenge (7kyu)
+
+const orderedCount = function (text) {
+    const splitStr = text.split('');
+      const str = [...new Set(splitStr)];
+      
+      let resultArr = [];
+      
+      for (let i = 0; i <= str.length-1; i += 1) {
+        resultArr.push([str[i], splitStr.filter((s) => s === str[i]).length]);
+      }
+      
+      return resultArr;
     }
-    return ans
-}
 
-console.log(countBy(2, 5))
+// console.log(orderedCount("abracadabra"))
 
 
