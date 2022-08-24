1f4a77f0fa6d35d4b3a35d19966b7eb35a0cbdda
Evan Shaw <e85.shaw@gmail.com>
2022-08-23 18:29:09 -0700

    First code challenge completed. @301!

--- aprilChallenges/js/aug2022/aug23rd.js
+++ aprilChallenges/js/aug2022/aug23rd.js
@@ -25,3 +25,5 @@
 
 // console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]))
 // console.log(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]))
+
+// @301: 
