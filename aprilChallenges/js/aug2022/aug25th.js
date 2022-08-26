7c1e66633081720113910616b943736e08af48ab
Evan Shaw <e85.shaw@gmail.com>
2022-08-25 20:10:19 -0700

    1st challenge completed

---
+++ aprilChallenges/js/aug2022/aug25th.js
@@ -0,0 +1,24 @@
+//***************************************************************
+// @294
+//  Grasshopper - Terminal game move function
+
+// moving from left to right, roll a set of dice and move the number of spaces indicated by the dice 2TIMES
+
+// create a function, taking two arguments. 
+// a position, and a roll. 
+// the position is where we start, and the roll is how many we move. 
+// using simple math, we could just add the position and the roll, squared and return the number. 
+
+// function move(position, roll) {
+//    return position + roll * 2    
+// }
+
+
+// const move = (position, roll) => position + roll * 2
+
+// console.log(move(3, 6))
+
+//***************************************************************
+// @293
+
+//
