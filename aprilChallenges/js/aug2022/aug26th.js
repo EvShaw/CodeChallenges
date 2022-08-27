d8f433cab64c252f670167fe6f257cd554867cc1
Evan Shaw <e85.shaw@gmail.com>
2022-08-26 20:00:44 -0700

    Daily challenge: 5, 6, 7, 8, and 9 completed.

--- aprilChallenges/js/aug2022/aug26th.js
+++ aprilChallenges/js/aug2022/aug26th.js
@@ -55,6 +55,70 @@
 // 5 Without Numbers!!
 
 // write a function that always returns 5 without using 0123456789*+-/
-    //return a string a string that is 5 characters long with the length property appended by dot notation.  
+//return a string a string that is 5 characters long with the length property appended by dot notation.  
 
-    // const unusualFive = _ => 'count'.length
+// const unusualFive = _ => 'count'.length
+
+
+//***************************************************************
+//BONUS CHALLENGES BECAUSE WHY NOT.... 
+
+//FizzBuzz
+// Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, "buzz" at multiples of 5 and "fizzbuzz" at multiples of 3 and 5
+
+//create a function, that accepts a number which defaults to 100. 
+//inside the function, create a loop. starting at 1 => num. 
+//within the loop, check if the number is a multiple of 3&5 first. This will need to go first before checking in the loop if its just a multiple of 3 or 5, so we don't skip anything. 
+// print fizz if the i = 3, buzz if i = 5, fizzbuzz if i = 3 & 5
+
+// const fizzBuzz = (num = 100) => {
+//     for(let i = 1; i <= num; i++) {
+//         if(i % 3 == 0 && i % 5 == 0) console.log('FizzBuzz')
+//         else if (i % 3 == 0) console.log('Fizz')
+//         else if (i % 5 == 0) console.log('Buzz')
+//         else console.log(i)
+//     }
+// }
+
+
+// fizzBuzz()
+
+//7 - inspiring strings
+
+// function longestWord(stringOfWords) {
+//     let longestWord = ''
+
+//     let strArr = stringOfWords.split(' ')
+
+//     strArr.forEach(word => {
+//         if (word.length >= longestWord.length) longestWord = word
+//     })
+
+//     return longestWord
+// }
+
+//7 kyu Convert an array of strings to array of numbers
+// function toNumberArray(stringarray) {
+//     return stringarray.map(el => +el)
+// }
+
+// const toNumberArray = stringarray => stringarray.map(el => +el)
+
+
+// console.log(toNumberArray(["1.1", "2.2", "3.3"]))
+
+//***************************************************************
+// @285 const binToDec = bin => parseInt(bin, 2)
+
+//***************************************************************
+//@284 
+    //If you can't sleep, just count sheep:
+    
+//const countSheep = num => num > 0 ? [...Array(num)].map((_, i) => `${i+1} sheep...`).join('') : '';
+
+
+//***************************************************************
+
+
+    
+
