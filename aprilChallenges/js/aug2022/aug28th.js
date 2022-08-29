e318dea4ba000bed19a3605f3f23d27857eb3078
Evan Shaw <e85.shaw@gmail.com>
2022-08-29 10:24:37 -0700

    Afterhours challenges saved

--- aprilChallenges/js/aug2022/aug28th.js
+++ aprilChallenges/js/aug2022/aug28th.js
@@ -21,7 +21,7 @@ function past(h, m, s) {
     return totalMilliseconds
 }
 
-console.log(past(0, 1, 0))
+// console.log(past(0, 1, 0))
 
 
 
@@ -46,27 +46,50 @@ function derive(coefficient, exponent) {
 }
 
 
- //***************************************************************
+//***************************************************************
 //@273 - Simple validation of a username with regex
 
-    //using regex, validate a username with only lowercase letters, numbers, and underscores. Length should be no less than 4 and no greater than 16
+//using regex, validate a username with only lowercase letters, numbers, and underscores. Length should be no less than 4 and no greater than 16
 
-    //inside the function, check the length and ensure it passes the min and max length allowed. Then google the regex needed and return it!
+//inside the function, check the length and ensure it passes the min and max length allowed. Then google the regex needed and return it!
 
-    const validateUsr = username => {
-        return username.length >= 4 && 
-          username.length <= 16 && 
-          (username.match(/[a-z]|_|\d/g).length === username.length);
-        }
+const validateUsr = username => {
+    return username.length >= 4 &&
+        username.length <= 16 &&
+        (username.match(/[a-z]|_|\d/g).length === username.length);
+}
 
- //***************************************************************
+//***************************************************************
 //@272: Get character from ASCII value
 
 //given a number, return the correspond ASCII char for that value. 
-    
+
 //inside the function, use the STRING fromCharCode method to convert the character to its UTF-16 value.
 
-function getChar(c){
+function getChar(c) {
     return String.fromCharCode(c)
-  }
+}
 
+//***************************************************************
+// random 7kyu: Remove all the marked elements of a list.
+
+//Define a method that removes from a given array of integers all the values contained in a second array. 
+// // [1, 1, 2 ,3 ,1 ,2 ,3 ,4], [1, 3] -> [2, 2, 4]
+// * [1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8], [1, 3, 4, 2] -> [5, 6, 7, 8]
+// * [8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3], [2, 4, 3] -> [8, 7, 6, 5, 1]
+
+// given two arrays, the first an integer_list and the second a values list, remove from the integer list all the numbers contained in the values_list so that the returned array only return numbers not on the value list. 
+
+
+// inside the function, 
+//create a variable that will capture the second argument and later used in the filter when extracting numbers.    
+//return map() 
+// map over the integer list, returning a new array
+// filter out any integers not on the values list. 
+// and return those values. 
+
+Array.prototype.remove_ = function (integer_list, values_list) {
+    return integer_list.filter( el => !values_list.includes(el))
+}
+
+console.log(Array.prototype.remove_([1, 1, 2 ,3 ,1 ,2 ,3 ,4,], [1, 3]))
