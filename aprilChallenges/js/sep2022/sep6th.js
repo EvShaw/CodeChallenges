5d5974cf1e771ecfcbd2406dbab7676e5f5301cb
Evan Shaw <e85.shaw@gmail.com>
2022-09-06 20:24:55 -0700

    3x Code Challenges completed: @242

---
+++ aprilChallenges/js/sep2022/sep6th.js
@@ -0,0 +1,57 @@
+//***************************************************************
+//@244: Get planet name by ID:
+
+
+// function getPlanetName(id){
+//     switch(id){
+//       case 1:
+//         return 'Mercury'
+//         break;
+//       case 2:
+//         return 'Venus'
+//          break;
+//       case 3:
+//         return 'Earth'
+//          break;
+//       case 4:
+//         return 'Mars'
+//          break;
+//       case 5:
+//         return 'Jupiter'
+//          break;
+//       case 6:
+//         return 'Saturn'
+//          break;
+//       case 7:
+//        return 'Uranus'
+//          break;
+//       case 8:
+//         return 'Neptune'
+//          break;
+//     }
+//   }
+
+  //***************************************************************
+//@243: Wilson Prime: 
+
+  // the three known wilson primes are: 5, 13, 563
+
+//   function amIWilson(p) {
+//     if (p === 5 || p === 13 || p === 563) {
+//     return true;
+//     }
+//     return false;
+    
+//     }
+
+    // const amIWilson = p => [5, 13, 563].indexOf(p) > -1
+
+  //***************************************************************
+//@242:
+
+
+    // function howMuchWater(w, l, c){
+    //     if(c>2*l) return 'Too much clothes'
+    //     if(c<l) return 'Not enough clothes'
+    //     return +(w * (1.1**(c-l))).toFixed(2)
+    //   }
