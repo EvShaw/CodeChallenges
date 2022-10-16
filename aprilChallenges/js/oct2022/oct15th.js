4072e3f042450d85c7da10bda63d32acade2fda2
Evan Shaw <e85.shaw@gmail.com>
2022-10-15 21:31:47 -0700

    Daily code challenge completed

---
+++ aprilChallenges/js/oct2022/oct15th.js
@@ -0,0 +1,13 @@
+const today = new Date;
+console.log(today.toLocaleString())
+
+
+// to return the type of sum using explict converstion, sum a and b, then return their typeof with the typeof operator
+
+// function typeOfSum(a, b) {
+//     let sum = a + b
+//     return typeof (a+b)
+// }
+
+// console.log(typeOfSum(12, 1))
+// console.log(typeOfSum(true, 1))
